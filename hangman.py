b17c4cb263ee27b65eccbf015ad6edd208643f07
CodesWithRobi <rayercodes@gmail.com>
2024-02-22 13:20:05 +0530

    Added some lines for testing

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+import string.h
 print('''
   😀
 🫲👕🫱
@@ -6,3 +7,8 @@ print('''
 ''')
 state = list(range(1,8))
 word = input()
+while True:
+    i = getch()
+    if i == 'e':
+        print("Done")
+        break
