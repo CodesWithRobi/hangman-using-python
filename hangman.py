beaba6e8ae0dd19a49edf38bee4821dfd9fe1dcc
CodesWithRobi <rayercodes@gmail.com>
2024-02-27 11:44:13 +0530

    Improved

--- hangman.py
+++ hangman.py
@@ -8,18 +8,11 @@ word = input()
 flag = 0
 pos = 8
 while pos > 1 and len(word) > 0:
-    temp = ""
     i = input()
-    for w in word:
-        if i == w:
-            print(i)
-            flag = 1
-        else:
-            temp += w
-    word = temp
-    if flag != 1:
+    count = word.count(i)
+    word = word.replace(i, '')
+    print(word)
+    if count == 0:
         pos -= 1
         print(pos)
-    flag = 0
-    
 print("DED")
