86443e9c9ba0d890237cea1db6a141f79eda5630
CodesWithRobi <rayercodes@gmail.com>
2024-02-28 10:14:36 +0530

    Updated

--- hangman.py
+++ hangman.py
@@ -4,14 +4,20 @@ print('''
   👖
   👟👟
 ''')
-word = input()
+s = input()
+word = s
 flag = 0
 pos = 8
 while pos > 1 and len(word) > 0:
     i = input()
     count = word.count(i)
-    word = word.replace(i, '')
-    print(word)
+    word = word.replace(i, '-')
+    for i in range(len(word)):
+        if s[i] == word[i]:
+            print('-', end='')
+        else:
+            print(s[i], end='')
+    print()
     if count == 0:
         pos -= 1
         print(pos)
