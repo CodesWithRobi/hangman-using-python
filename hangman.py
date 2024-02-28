be9c6b88d967675eca51f9104ad3e8758918ad20
CodesWithRobi <rayercodes@gmail.com>
2024-02-28 13:56:47 +0530

    Optimized code and made it stable

--- hangman.py
+++ hangman.py
@@ -1,22 +1,25 @@
-print('''
+from os import system, name
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+man = '''
   😀
 🫲👕🫱
   👖
   👟👟
-''')
+'''
 s = input()
 word = s
 flag = 0
 pos = 8
 while pos > 1 and len(word) > 0:
     i = input()
+    clear()
     count = word.count(i)
     word = word.replace(i, '-')
-    for i in range(len(word)):
-        if s[i] == word[i]:
-            print('-', end='')
-        else:
-            print(s[i], end='')
+    print(''.join(['-' if word[i] != '-' else s[i] for i in range(len(word))]))
     print()
     if count == 0:
         pos -= 1
